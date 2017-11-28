090dcdba70adb61f516e8c9ffb2e2fa0a178570f
Tiago Lopo Da Silva <tlopo@ibm.com>
2017-11-28 17:42:17 +0100

    Cleaned code a bit

--- src/main/groovy/org/tiagolopo/Erb.groovy
+++ src/main/groovy/org/tiagolopo/Erb.groovy
@@ -1,15 +1,9 @@
 package org.tiagolopo
 
-import groovy.json.JsonOutput
 import groovyx.net.http.RESTClient
-import org.jruby.embed.ScriptingContainer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.*
-import org.tiagolopo.utils.Deflatter
 import org.tiagolopo.utils.ErbParser
-import org.tiagolopo.utils.Flatter
-import org.yaml.snakeyaml.DumperOptions
-import org.yaml.snakeyaml.Yaml
 
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -33,10 +27,8 @@ class Erb {
 
         label = label?: 'master'
 
-        println("---------------> ${erbFile}")
         def databag = getDataBag(appName,profiles,label)
 
-        //def template = 'var = <%=node["curl"]["config"]["retries"]%>'
         def template = getTemplate(label, erbFile)
 
         def parsed = ErbParser.parse(databag,template)
