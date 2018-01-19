315c7dc1dd9f4d3f250bb0bc9580a354324b86c4
tlopo <tlopo@ibm.com>
2018-01-19 14:53:06 +0000

    Use self signed certificate

--- scripts/transcrypt-init.sh
+++ scripts/transcrypt-init.sh
@@ -7,3 +7,12 @@ mkdir "$CHECKOUT_DIR" -p
 git clone "$GIT_URL" "$CHECKOUT_DIR"
 
 ( cd "$CHECKOUT_DIR" && transcrypt -c aes-256-cbc -p "$TRANSCRYPT_KEY" <<< $'\ny\n')
+
+(
+  mkdir /opt/ssl &&
+  cd /opt/ssl && 
+  /opt/jdk/bin/keytool -genkey \
+    -alias configserver -storetype PKCS12 -keyalg RSA -keysize 2048 \
+    -keystore keystore.p12 \
+    -validity 3650 <<< $'abc123\nabc123\nTiago Lopo\nConfigServer\nConfigServer\n\n\nPL\nyes\n'
+)
