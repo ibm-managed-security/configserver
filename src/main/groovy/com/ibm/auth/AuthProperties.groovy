e70fc80f2e77e16c3bf7417b175be78f6ebb03a2
Kaleb Walton <kdwalton@us.ibm.com>
2018-03-26 15:14:47 -0400

    logging

--- src/main/groovy/com/ibm/auth/AuthProperties.groovy
+++ src/main/groovy/com/ibm/auth/AuthProperties.groovy
@@ -1,5 +1,7 @@
 package com.ibm.auth
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
@@ -9,9 +11,11 @@ import org.springframework.core.io.FileSystemResource
 import org.springframework.core.io.Resource
 import org.springframework.stereotype.Component
 
+
 @Component
 @ConfigurationProperties
 class AuthProperties {
+    private static final Logger logger = LoggerFactory.getLogger(AuthProperties.class)
 
     private List<AuthUser> users = new ArrayList<>();
 
@@ -33,10 +37,13 @@ class AuthProperties {
             if (new File(authYmlPath)) {
                 authYmlResource = new FileSystemResource(authYmlPath)
             }
+            logger.info("Using ${authYmlResource.getPath()} for authentication configuration")
             yaml.setResources(authYmlResource)
             propertySourcesPlaceholderConfigurer.setProperties(yaml.getObject())
         } catch (IllegalStateException e) {
             // do nothing
+        } catch (Exception e) {
+            e.printStackTrace()
         }
         return propertySourcesPlaceholderConfigurer;
     }
